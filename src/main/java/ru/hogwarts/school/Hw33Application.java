97a341dc2248bb7e23f556d6be681a15455a245a
nohero_ <serega1552@yandex.ru>
2022-07-25 10:28:17 +0400

    Init commit

---
+++ src/main/java/ru/hogwarts/school/Hw33Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw33Application.class, args);
+    }
+
+}
